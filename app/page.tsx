c58ad28e910c213b3c8123e2d32b868968cc3b3a
mirza-sync <mirzasyahmi123@gmail.com>
2026-03-25 22:47:05 +0800

    Implement nextjs font optimization

--- app/page.tsx
+++ app/page.tsx
@@ -2,7 +2,7 @@ import Image from "next/image";
 
 export default function Home() {
   return (
-    <div className="flex min-h-screen items-center justify-center bg-zinc-50 font-sans dark:bg-black">
+    <div className="flex min-h-screen items-center justify-center bg-zinc-50 dark:bg-black">
       <main className="flex min-h-screen w-full max-w-3xl flex-col items-center justify-between bg-white px-16 py-32 sm:items-start dark:bg-black">
         <Image className="dark:invert" src="/next.svg" alt="Next.js logo" width={100} height={20} priority />
         <div className="flex flex-col items-center gap-6 text-center sm:items-start sm:text-left">
