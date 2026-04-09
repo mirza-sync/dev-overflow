b244dd9b06e2d6207b11b0e9a0f3fe3f0473b26c
mirza-sync <mirzasyahmi123@gmail.com>
2026-04-09 23:59:25 +0800

    Add bg image on auth layout

--- app/(auth)/layout.tsx
+++ app/(auth)/layout.tsx
@@ -1,5 +1,5 @@
 const AuthLayout = ({ children }: { children: React.ReactNode }) => {
-  return <main>{children}</main>;
+  return <main className="background-image-auth-light dark:background-image-auth-dark flex">{children}</main>;
 };
 
 export default AuthLayout;
