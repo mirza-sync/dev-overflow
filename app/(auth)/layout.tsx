1ade72e71b5b6d89a737144e9a8520955f1bd476
mirza-sync <mirzasyahmi123@gmail.com>
2026-04-10 00:10:21 +0800

    Fix bg theme reference not working

--- app/(auth)/layout.tsx
+++ app/(auth)/layout.tsx
@@ -1,5 +1,5 @@
 const AuthLayout = ({ children }: { children: React.ReactNode }) => {
-  return <main className="background-image-auth-light dark:background-image-auth-dark flex">{children}</main>;
+  return <main className="bg-auth-light dark:bg-auth-dark flex">{children}</main>;
 };
 
 export default AuthLayout;
