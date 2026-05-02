a2648fddad850568eb29943218855f4ece516967
mirza-sync <mirzasyahmi123@gmail.com>
2026-05-02 23:55:27 +0800

    Add sidebar links

---
+++ constants/index.ts
@@ -0,0 +1,39 @@
+import ROUTES from "./routes";
+
+export const sidebarLinks = [
+  {
+    imgURL: "/icons/home.svg",
+    route: "/",
+    label: ROUTES.HOME,
+  },
+  {
+    imgURL: "/icons/users.svg",
+    route: "/community",
+    label: "Community",
+  },
+  {
+    imgURL: "/icons/star.svg",
+    route: "/collection",
+    label: "Collections",
+  },
+  {
+    imgURL: "/icons/suitcase.svg",
+    route: "/jobs",
+    label: "Find Jobs",
+  },
+  {
+    imgURL: "/icons/tag.svg",
+    route: "/tags",
+    label: "Tags",
+  },
+  {
+    imgURL: "/icons/user.svg",
+    route: "/profile",
+    label: "Profile",
+  },
+  {
+    imgURL: "/icons/question.svg",
+    route: "/ask-question",
+    label: "Ask a question",
+  },
+];
