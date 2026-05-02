ce0723a02c48d466b3fd55d0365716c028e23791
mirza-sync <mirzasyahmi123@gmail.com>
2026-05-02 23:50:17 +0800

    Add login button on mobile nav

--- components/navigation/navbar/MobileNavigation.tsx
+++ components/navigation/navbar/MobileNavigation.tsx
@@ -44,6 +44,24 @@ const MobileNavigation = () => {
               <p>Nav Links</p>
             </section>
           </SheetClose>
+
+          <div className="flex flex-col gap-3">
+            <SheetClose asChild>
+              <Link href={ROUTES.SIGN_IN}>
+                <Button className="small-medium btn-secondary min-h-[41px] w-full rounded-lg px-4 py-3 shadow-none">
+                  <span className="primary-text-gradient">Log In</span>
+                </Button>
+              </Link>
+            </SheetClose>
+
+            <SheetClose asChild>
+              <Link href={ROUTES.SIGN_UP}>
+                <Button className="small-medium btn-tertiary light-border-2 text-dark400_light900 min-h-[41px] w-full rounded-lg px-4 py-3 shadow-none">
+                  Sign Up
+                </Button>
+              </Link>
+            </SheetClose>
+          </div>
         </div>
       </SheetContent>
     </Sheet>
