cadc41d5634a46a23d059de179c21a00d2153a44
mirza-sync <mirzasyahmi123@gmail.com>
2026-05-04 23:47:33 +0800

    Add auth buttons to left sidebar

--- components/navigation/LeftSidebar.tsx
+++ components/navigation/LeftSidebar.tsx
@@ -1,11 +1,49 @@
 import React from "react";
 import NavLinks from "./navbar/NavLinks";
+import Link from "next/link";
+import { Button } from "../ui/button";
+import ROUTES from "@/constants/routes";
+import Image from "next/image";
 
 const LeftSidebar = () => {
   return (
     <section className="custom-scrollbar background-light900_dark200 light-border shadow-light-300 sticky top-0 left-0 flex h-screen flex-col justify-between overflow-y-auto border-r p-6 pt-36 max-sm:hidden lg:w-66.5 dark:shadow-none">
       <div className="flex flex-1 flex-col gap-6">
         <NavLinks />
+      </div>
+
+      <div className="flex flex-col gap-3">
+        <Button
+          asChild
+          className="small-medium btn-secondary min-h-10.25 w-full rounded-lg px-4 py-3 shadow-none"
+        >
+          <Link href={ROUTES.SIGN_IN}>
+            <Image
+              src="/icons/account.svg"
+              alt="Account"
+              width={20}
+              height={20}
+              className="invert-colors lg:hidden"
+            />
+            <span className="primary-text-gradient max-lg:hidden">Log In</span>
+          </Link>
+        </Button>
+
+        <Button
+          asChild
+          className="small-medium btn-tertiary light-border-2 text-dark400_light900 min-h-10.25 w-full rounded-lg px-4 py-3 shadow-none"
+        >
+          <Link href={ROUTES.SIGN_UP}>
+            <Image
+              src="/icons/sign-up.svg"
+              alt="Sign Up"
+              width={20}
+              height={20}
+              className="invert-colors lg:hidden"
+            />
+            <span className="primary-text-gradient max-lg:hidden">Sign Up</span>
+          </Link>
+        </Button>
       </div>
     </section>
   );
