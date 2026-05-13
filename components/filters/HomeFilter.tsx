c40491fb471a1709ba3a407ad7dbadad7258fbb3
mirza-sync <mirzasyahmi123@gmail.com>
2026-05-13 23:14:16 +0800

    Add filter buttons

--- components/filters/HomeFilter.tsx
+++ components/filters/HomeFilter.tsx
@@ -1,5 +1,25 @@
+import { Button } from "../ui/button";
+
+const filters = [
+  { name: "Newest", value: "newest" },
+  { name: "Popular", value: "popular" },
+  { name: "Unanswered", value: "unanswered" },
+  { name: "Recommended", value: "recommended" },
+];
+
 const HomeFilter = () => {
-  return <div>HomeFilter</div>;
+  return (
+    <div className="mt-10 hidden flex-wrap gap-3 sm:flex">
+      {filters.map((filter) => (
+        <Button
+          key={filter.value}
+          className="body-medium rounded-lg px-6 py-3 capitalize shadow-none"
+        >
+          {filter.name}
+        </Button>
+      ))}
+    </div>
+  );
 };
 
 export default HomeFilter;
