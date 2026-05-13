fa189a7f0cd7d88bd68ed628709cc14375930c14
mirza-sync <mirzasyahmi123@gmail.com>
2026-05-13 23:59:20 +0800

    Implement filter logic

--- components/filters/HomeFilter.tsx
+++ components/filters/HomeFilter.tsx
@@ -1,4 +1,10 @@
+"use client";
+
+import { useState } from "react";
 import { Button } from "../ui/button";
+import { useRouter, useSearchParams } from "next/navigation";
+import { cn } from "@/lib/utils";
+import { formUrlQuery, removeKeysFromUrlQuery } from "@/lib/url";
 
 const filters = [
   { name: "Newest", value: "newest" },
@@ -8,12 +14,43 @@ const filters = [
 ];
 
 const HomeFilter = () => {
+  const router = useRouter();
+  const searchParams = useSearchParams();
+  const filterParams = searchParams.get("filter") || "";
+  const [active, setActive] = useState(filterParams || "");
+
+  const handleFilterChange = (filterValue: string) => {
+    let newUrl = "`";
+
+    if (filterValue === active) {
+      setActive("");
+      newUrl = removeKeysFromUrlQuery({
+        params: filterParams.toString(),
+        keysToRemove: ["filter"],
+      });
+    } else {
+      setActive(filterValue);
+      newUrl = formUrlQuery({
+        params: filterParams.toString(),
+        key: "filter",
+        value: filterValue,
+      });
+    }
+    router.push(newUrl, { scroll: false });
+  };
+
   return (
     <div className="mt-10 hidden flex-wrap gap-3 sm:flex">
       {filters.map((filter) => (
         <Button
           key={filter.value}
-          className="body-medium rounded-lg px-6 py-3 capitalize shadow-none"
+          className={cn(
+            "body-medium rounded-lg px-6 py-3 capitalize shadow-none",
+            active === filter.value
+              ? "bg-primary-100 text-primary-500 dark:bg-dark-400"
+              : "bg-light-800 text-light-500 dark:bg-dark-300 dark:text-light-500"
+          )}
+          onClick={() => handleFilterChange(filter.value)}
         >
           {filter.name}
         </Button>
