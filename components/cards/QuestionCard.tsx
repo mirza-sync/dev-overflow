804d40bd939c25c30245a33c6b472793fc2931b8
mirza-sync <mirzasyahmi123@gmail.com>
2026-05-19 23:53:55 +0800

    Show question title and time ago

--- components/cards/QuestionCard.tsx
+++ components/cards/QuestionCard.tsx
@@ -1,4 +1,6 @@
+import ROUTES from "@/constants/routes";
 import { getTimeAgo } from "@/lib/utils";
+import Link from "next/link";
 
 interface QuestionCardProps {
   question: Question;
@@ -11,7 +13,14 @@ const QuestionCard = ({
     <div className="card-wrapper rounded-[10px] p-9 sm:px-11">
       <div className="flex flex-col-reverse items-start justify-between gap-5 sm:flex-row">
         <div>
-          <span>{getTimeAgo(createdAt)}</span>
+          <span className="subtle-regular text-dark400_light700 line-clamp-1 flex sm:hidden">
+            {getTimeAgo(createdAt)}
+          </span>
+          <Link href={ROUTES.QUESTION(id)}>
+            <h3 className="sm:h3-semibold base-semibold text-dark200_light900 line-clamp-1 flex-1">
+              {title}
+            </h3>
+          </Link>
         </div>
       </div>
     </div>
