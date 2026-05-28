b32c8255b63896f0bd1fac8b958ecf78736aa1bb
mirza-sync <mirzasyahmi123@gmail.com>
2026-05-28 23:59:24 +0800

    Add other fields

--- components/forms/QuestionForm.tsx
+++ components/forms/QuestionForm.tsx
@@ -4,7 +4,7 @@ import { AskQuestionSchema } from "@/lib/validations";
 import { zodResolver } from "@hookform/resolvers/zod";
 import { Controller, useForm } from "react-hook-form";
 import z from "zod";
-import { Field, FieldLabel, FieldError } from "../ui/field";
+import { Field, FieldLabel, FieldError, FieldDescription } from "../ui/field";
 import { Input } from "../ui/input";
 
 const QuestionForm = () => {
@@ -42,6 +42,63 @@ const QuestionForm = () => {
                 className="paragraph-regular background-light900_dark300 light-border-2 text-dark300_light700 no-focus min-h-14 border"
               />
               {fieldState.invalid && <FieldError errors={[fieldState.error]} />}
+              <FieldDescription className="body-regular text-light-500 mt-2.5">
+                Be specific and imagine you’re asking a question to another
+                person.
+              </FieldDescription>
+            </Field>
+          )}
+        />
+
+        <Controller
+          key="content"
+          name="content"
+          control={form.control}
+          render={({ field, fieldState }) => (
+            <Field data-invalid={fieldState.invalid}>
+              <FieldLabel
+                htmlFor={field.name}
+                className="paragraph-medium text-dark400_light800"
+              >
+                Detailed explanation of your problem{" "}
+                <span className="text-primary-500">*</span>
+              </FieldLabel>
+              {/* Editor */}
+              {fieldState.invalid && <FieldError errors={[fieldState.error]} />}
+              <FieldDescription className="body-regular text-light-500 mt-2.5">
+                Introduce the problem and expand on what you put in the title.
+              </FieldDescription>
+            </Field>
+          )}
+        />
+
+        <Controller
+          key="tags"
+          name="tags"
+          control={form.control}
+          render={({ field, fieldState }) => (
+            <Field data-invalid={fieldState.invalid}>
+              <FieldLabel
+                htmlFor={field.name}
+                className="paragraph-medium text-dark400_light800"
+              >
+                Tags <span className="text-primary-500">*</span>
+              </FieldLabel>
+              <div>
+                <Input
+                  {...field}
+                  id={field.name}
+                  type="text"
+                  aria-invalid={fieldState.invalid}
+                  placeholder="Add tags..."
+                  className="paragraph-regular background-light900_dark300 light-border-2 text-dark300_light700 no-focus min-h-14 border"
+                />
+              </div>
+              {fieldState.invalid && <FieldError errors={[fieldState.error]} />}
+              <FieldDescription className="body-regular text-light-500 mt-2.5">
+                Be specific and imagine you’re asking a question to another
+                person.
+              </FieldDescription>
             </Field>
           )}
         />
