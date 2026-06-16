ee1fd9f0e3fe22f2edb1a586374018e167bf8982
mirza-sync <mirzasyahmi123@gmail.com>
2026-06-16 23:11:56 +0800

    Add password field

--- schema/user.model.ts
+++ schema/user.model.ts
@@ -16,6 +16,7 @@ const UserSchema = new Schema(
     name: { type: String, required: true },
     username: { type: String, required: true },
     email: { type: String, required: true, unique: true },
+    password: { type: String, required: true },
     bio: { type: String },
     image: { type: String, required: true },
     location: { type: String },
