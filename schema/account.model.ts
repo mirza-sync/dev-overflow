f1cd345826fa5585510d59757da146721b923001
mirza-sync <mirzasyahmi123@gmail.com>
2026-06-17 23:34:41 +0800

    Move password from user to account

--- schema/account.model.ts
+++ schema/account.model.ts
@@ -3,6 +3,7 @@ import { model, models, Schema, Types } from "mongoose";
 export interface IAccount {
   userId: Types.ObjectId;
   name: string;
+  password?: string; // Password is optional since it was not needed in OAuth accounts
   provider: string;
   providerAccountId: string;
 }
@@ -10,6 +11,7 @@ export interface IAccount {
 const AccountSchema = new Schema<IAccount>({
   userId: { type: Schema.Types.ObjectId, ref: "User", required: true },
   name: { type: String, required: true },
+  password: { type: String },
   provider: { type: String, required: true },
   providerAccountId: { type: String, required: true },
 });
--- schema/user.model.ts
+++ schema/user.model.ts
@@ -16,7 +16,6 @@ const UserSchema = new Schema(
     name: { type: String, required: true },
     username: { type: String, required: true },
     email: { type: String, required: true, unique: true },
-    password: { type: String, required: true },
     bio: { type: String },
     image: { type: String, required: true },
     location: { type: String },
