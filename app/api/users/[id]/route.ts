542f0bb1952b8598a13d81df75f0eeed48a897b7
mirza-sync <mirzasyahmi123@gmail.com>
2026-08-22 23:55:20 +0800

    Add delete user endpoint

--- app/api/users/[id]/route.ts
+++ app/api/users/[id]/route.ts
@@ -16,8 +16,29 @@ export async function GET(
 
   try {
     await connectDb();
-    const user = await User.findById();
+    const user = await User.findById(id);
 
+    if (!user) {
+      throw NotFoundError("User");
+    }
+    return NextResponse.json({ success: true, data: user }, { status: 200 });
+  } catch (error) {
+    return handleError(error, "api") as ErrorResponse;
+  }
+}
+
+export async function DELETE(
+  _: Request,
+  { params }: { params: Promise<{ id: string }> }
+) {
+  const { id } = await params;
+  if (!id) {
+    throw NotFoundError("User");
+  }
+
+  try {
+    await connectDb();
+    const user = await User.findByIdAndDelete(id);
     if (!user) {
       throw NotFoundError("User");
     }
