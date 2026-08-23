59fe454fe3978add779217b77d0d72273da87661
mirza-sync <mirzasyahmi123@gmail.com>
2026-08-23 23:46:36 +0800

    Add update user endpoint

--- app/api/users/[id]/route.ts
+++ app/api/users/[id]/route.ts
@@ -1,6 +1,7 @@
 import handleError from "@/lib/handlers/errors";
 import { NotFoundError } from "@/lib/http-errors";
 import connectDb from "@/lib/mongoose";
+import { UserSchema } from "@/lib/validations";
 import User from "@/schema/user.model";
 import { ErrorResponse } from "@/types/global";
 import { NextResponse } from "next/server";
@@ -43,6 +44,36 @@ export async function DELETE(
       throw NotFoundError("User");
     }
     return NextResponse.json({ success: true, data: user }, { status: 200 });
+  } catch (error) {
+    return handleError(error, "api") as ErrorResponse;
+  }
+}
+
+export async function PATCH(
+  request: Request,
+  { params }: { params: Promise<{ id: string }> }
+) {
+  const { id } = await params;
+  if (!id) {
+    throw NotFoundError("User");
+  }
+
+  try {
+    await connectDb();
+    const body = await request.json();
+    const validatedData = UserSchema.partial().parse(body);
+
+    const updatedUser = await User.findByIdAndUpdate(id, validatedData, {
+      new: true,
+    });
+    if (!updatedUser) {
+      throw NotFoundError("User");
+    }
+
+    return NextResponse.json(
+      { success: true, data: updatedUser },
+      { status: 200 }
+    );
   } catch (error) {
     return handleError(error, "api") as ErrorResponse;
   }
